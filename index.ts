cc11856a14c76fc6a8b7584799cf7fc705131eeb
Julien <julien@ripouteau.com>
2023-07-24 15:18:05 +0200

    refactor: don't export defenses from main entrypoint

--- index.ts
+++ index.ts
@@ -13,5 +13,3 @@ export { defineConfig } from './src/define_config.js'
 export { stubsRoot } from './stubs/main.js'
 export { configure } from './configure.js'
 export * as errors from './src/exceptions.js'
-
-export * from './src/defenses/index.js'
